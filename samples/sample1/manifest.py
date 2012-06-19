562c7c0b96a48f3fc0528aceffdcd86a79865116
Dong-seob Park <dongseob.park@gmail.com>
2012-06-20 02:24:56 +0900

    ...

--- samples/sample1/manifest.py
+++ samples/sample1/manifest.py
@@ -2,6 +2,7 @@ from accelerando.core import manifest
 import handler1, handler2
 
 APPLICATION_CONTEXT = manifest(
+		hostname='211.189.127.88',
 		title='Blog Application', 
 		wsgi_handlers={
 			'handler1': handler1.application,
