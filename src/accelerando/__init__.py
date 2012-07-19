716e786ae16cf3abc68e9657cd42c2790de26774
Dongseob Park <joonjeong@leningrad.(none)>
2012-07-19 22:20:55 +0900

    new model's directory structure

---
+++ src/accelerando/__init__.py
@@ -0,0 +1,11 @@
+VERSION = (0, 1)
+
+def version():
+    return VERSION.join(".")
+
+def release():
+    return "0.1"
+
+import accelerando.auth
+import accelerando.auth.digest
+import accelerando.session
