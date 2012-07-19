79827a75d8fe6c9e8b701a712428a48292f66d21
Dongseob Park <joonjeong@leningrad.(none)>
2012-07-20 00:25:11 +0900

    modify setup file and direcoty names

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 from distutils.core import setup
 
 setup(
-		name='accelerando',
-		version='0.1',
-		description='Web Application Server supports Python3, PEP3333',
-		author='Dong-seob Park',
-		author_email='dongseob.park@gmail.com',
-		url='http://github.com/Dongseob-Park/accelerando',
-		package_dir={'': 'src'},
-		packages=['accelerando', 'accelerando.dispatcher', 'accelerando.processor', 'accelerando.util'],
-		data_files=[
-			('/usr/bin', ['bin/accelerando'])
-		]
+    name='accelerando',
+    version='0.1',
+    description='Web Application Server supports Python3, PEP3333',
+    author='Dong-seob Park',
+    author_email='dongseob.park@gmail.com',
+    url='http://github.com/Dongseob-Park/accelerando',
+    package_dir={'': 'lib'},
+    packages=['wsgi', 'wsgi.auth', 'wsgi.auth.digest', 'wsgi.session'],
+    data_files=[
+        ('/usr/bin', ['bin/wsgiup'])
+        ]
 )
